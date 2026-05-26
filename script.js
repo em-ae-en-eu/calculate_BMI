e457e44c68be3b0e4403527c05dd3896d1614e1a
manu <12345678+username@users.noreply.github.com>
2026-05-26 10:13:15 +0530

    Loop-asking user to enter num greater than 100

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+
+
 let weight = parseFloat(prompt('Enter your weight in KG'));
 let height = parseInt(prompt('Enter your height in cm'));
 
@@ -30,3 +32,26 @@ function calculate_BMI() {
 }
 
 calculate_BMI();
+
+
+
+// loop that asked user to enter number greater than 100. 
+
+
+let num;
+
+while(true){
+
+    num =  prompt("Enter a number greater than 100");
+
+    if(num === null || num === ''){
+        console.log("Try again"); break;
+    }else if(num >100){
+        console.log('The number is greater than 100'); break;
+    }else{
+        console.log('Try again');
+    }
+
+}
+
+
